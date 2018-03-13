cedd9e997c4cc5edd81d6643d76ba67df7e6f304
Trajche <tmichov@rubixcubesolutions.com>
2018-03-13 19:50:06 +0100

    frontend jeeeee

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,89 +5,64 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>BuilderBuddies</title>
+        <!-- Bootstrap css -->
+        <link href="{{ asset('bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+        <link href="{{ asset('bootstrap/css/bootstrap-reboot.min.css') }}" rel="stylesheet">
+        <link href="{{ asset('bootstrap/css/bootstrap-grid.min.css') }}" rel="stylesheet">
+        <!-- Custom css -->
+        <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+
+        <!-- Bootstrap js -->
+        <script type="text/javascript" src="{{ asset('bootstrap/js/bootstrap.min.js') }}"></script>
+
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
+        <div class="container-fluid" id="login-page">
+            <div class="row">
+                <div class="col-md-4 align-self-center" id="left-column">
+                    <div id="logo-wrapper">
+                        <img src="{{ asset('images/logo.png') }}" alt="Builder-Buddies-Logo">
+                    </div>
+                    <form method="POST" action="{{ route('login') }}" id="login-form">
+                        @csrf
+                        <div class="form-group">
+                            <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus placeholder="Email">
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
+                            @if ($errors->has('email'))
+                                <span class="invalid-feedback">
+                                    <strong>{{ $errors->first('email') }}</strong>
+                                </span>
+                            @endif
+                       
+                            <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                            @if ($errors->has('password'))
+                                <span class="invalid-feedback">
+                                    <strong>{{ $errors->first('password') }}</strong>
+                                </span>
+                            @endif
+                        
+                            <button type="submit">
+                                Login
+                            </button>
+
+                            <br>
+
+                            <a href="{{ route('password.request') }}">
+                                Forgot Your Password?
+                            </a>
+                            <a href="{{ route('register') }}">
+                                Register
+                            </a>
+                        </div>
+                    </form>
+                </div>
+                <div class="col-md-8" id="right-column">
+                    a
                 </div>
             </div>
         </div>
