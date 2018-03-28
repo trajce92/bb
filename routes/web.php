d6e941960cdbfa30bfe17d7017051780e7aaf3f5
mariashakleva <maria.shakleva@gmail.com>
2018-03-28 10:40:14 +0200

    post & comments section

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\IsUserLoggedIn;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,7 +14,7 @@
 
 Route::get('/', function () {
     return view('auth.login');
-});
+})->middleware(IsUserLoggedIn::Class);
 
 Auth::routes();
 
@@ -22,3 +23,4 @@ Route::resource('/{id}/notes', 'NotesController');
 Route::get('/{id}/profile', 'UserController@index');
 Route::post('/{id}/profile', 'UserController@editUser');
 Route::resource('/{id}/posts', 'PostsController');
+Route::resource('/{id}/comments', 'CommentsController');
